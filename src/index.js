92a7140c2439e9d1ee79f25ea3d5161c3389282b
Kirill Vishenkov <vishkir1991@gmail.com>
2017-08-07 16:29:45 +0300

    init #2

--- src/index.js
+++ src/index.js
@@ -39,6 +39,7 @@ export default () => {
     if (req.body && typeof req.body === 'object' && '_method' in req.body) {
       return req.body._method;
     }
+    return req;
   }));
   app.use(serve(path.join(__dirname, '..', 'public')));
 
