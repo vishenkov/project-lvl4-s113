d988d16d4afd711ea75ad2a8176453aa81dff42b
Kirill Vishenkov <vishkir1991@gmail.com>
2017-08-07 16:20:16 +0300

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# task manager (nodejs-package)
+
+[![Code Climate](https://codeclimate.com/github/vishenkov/project-lvl4-s113/badges/gpa.svg)](https://codeclimate.com/github/vishenkov/project-lvl4-s113)
+[![Issue Count](https://codeclimate.com/github/vishenkov/project-lvl4-s113/badges/issue_count.svg)](https://codeclimate.com/github/vishenkov/project-lvl4-s113)
+[![Build Status](https://travis-ci.org/vishenkov/project-lvl4-s113.svg?branch=master)](https://travis-ci.org/vishenkov/project-lvl4-s113)
+
+## Setup
+
+```
+make install
+```
+
---
+++ src/index.js
@@ -0,0 +1,82 @@
+// @flow
+
+import 'babel-polyfill';
+
+import path from 'path';
+import Koa from 'koa';
+import Pug from 'koa-pug';
+import Router from 'koa-router';
+import koaLogger from 'koa-logger';
+import serve from 'koa-static';
+import middleware from 'koa-webpack';
+import bodyParser from 'koa-bodyparser';
+import session from 'koa-generic-session';
+import flash from 'koa-flash-simple';
+import _ from 'lodash';
+import methodOverride from 'koa-methodoverride';
+
+import rollbar from './lib/rollbar';
+import getWebpackConfig from '../webpack.config.babel';
+import addRoutes from './controllers';
+import container from './container';
+
+
+export default () => {
+  const app = new Koa();
+
+  app.keys = ['some secret hurr'];
+  app.use(session(app));
+  app.use(flash());
+  app.use(async (ctx, next) => {
+    ctx.state = {
+      flash: ctx.flash,
+      isSignedIn: () => ctx.session.userId !== undefined,
+    };
+    await next();
+  });
+  app.use(bodyParser());
+  app.use(methodOverride((req) => {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      return req.body._method;
+    }
+  }));
+  app.use(serve(path.join(__dirname, '..', 'public')));
+
+  app.use(async (ctx, next) => {
+    try {
+      await next();
+    } catch (err) {
+      rollbar.log(err.message);
+      ctx.status = err.status || 500;
+      ctx.body = err.message;
+      ctx.app.emit('error', err, ctx);
+    }
+  });
+
+  if (process.env.NODE_ENV !== 'test') {
+    app.use(middleware({
+      config: getWebpackConfig(),
+    }));
+  }
+
+  app.use(koaLogger());
+  const router = new Router();
+  addRoutes(router, container);
+  app.use(router.allowedMethods());
+  app.use(router.routes());
+
+  const pug = new Pug({
+    viewPath: path.join(__dirname, 'views'),
+    debug: true,
+    pretty: true,
+    compileDebug: true,
+    locals: [],
+    basedir: path.join(__dirname, 'views'),
+    helperPath: [
+      { _ },
+      { urlFor: (...args) => router.url(...args) },
+    ],
+  });
+  pug.use(app);
+  return app;
+};
