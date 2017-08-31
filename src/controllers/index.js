afeea2c34dd7d1fc76b16207c0e44d8371285066
Kirill Vishenkov <vishkir1991@gmail.com>
2017-08-31 11:23:57 +0300

    fix tasks routing

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,11 +2,10 @@ import welcome from './welcome';
 import users from './users';
 import sessions from './sessions';
 import profile from './profile';
-import task from './task';
 import tasks from './tasks';
 import tags from './tags';
 
 
-const controllers = [welcome, users, sessions, profile, task, tasks, tags];
+const controllers = [welcome, users, sessions, profile, tasks, tags];
 
 export default (router, container) => controllers.forEach(f => f(router, container));
--- src/controllers/tasks.js
+++ src/controllers/tasks.js
@@ -1,7 +1,9 @@
 import _ from 'lodash';
 import buildFormObj from '../lib/formObjectBuilder';
+import fsm from '../lib/fsmTaskStatus';
 
 export default (router, { Task, User, Tag, TaskStatus, logger }) => {
+  const log = logger('tasks');
   router
     .get('tasks', '/tasks', async (ctx) => {
       const tasks = await Task.findAll({
@@ -38,10 +40,10 @@ export default (router, { Task, User, Tag, TaskStatus, logger }) => {
       ctx.render('tasks/new', { f: buildFormObj(task), users, tags });
     })
 
-    .get('tasksByStatus', '/tasks/status/:status', async (ctx) => {
+    .get('tasksByStatus', '/tasks/status/:id', async (ctx) => {
       const status = await TaskStatus.findOne({
         where: {
-          name: ctx.params.status,
+          id: ctx.params.id,
         },
       });
       if (!status) {
@@ -59,6 +61,93 @@ export default (router, { Task, User, Tag, TaskStatus, logger }) => {
         ],
       });
       ctx.render('tasks/status', { tasks });
+    })
+
+    .get('editTask', '/tasks/:id/edit', async (ctx) => {
+      if (!ctx.state.isSignedIn()) {
+        ctx.throw(401);
+        return;
+      }
+      const task = await Task.findOne({
+        include: [
+          { model: User, as: 'creator' },
+          { model: User, as: 'assignedTo' },
+          { model: TaskStatus, as: 'status' },
+        ],
+        where: {
+          id: ctx.params.id,
+        },
+      });
+      if (task) {
+        const rawUsers = await User.findAll();
+        const users = rawUsers.map(user => ({
+          value: user.id,
+          text: user.id === ctx.session.userId ? '>> me <<' : user.fullName,
+          selected: task.assignedToId === user.id,
+        }));
+
+        const taskTags = await task.getTags();
+        const tagIds = taskTags.map(tag => tag.id);
+        const otherTags = await Tag.findAll({
+          where: {
+            id: {
+              $notIn: tagIds,
+            },
+          },
+        });
+        let tags = taskTags.map(tag => ({
+          text: tag.name,
+          value: tag.id,
+          checked: true,
+        }));
+        tags = otherTags.reduce((acc, tag) => ([
+          ...acc,
+          {
+            text: tag.name,
+            value: tag.id,
+          },
+        ]), tags);
+
+        log('Prepared tags: %o', tags);
+
+        const availableStates = fsm(task.status.name).transitions();
+        log('Available states: %o', availableStates);
+        const availableStatuses = await TaskStatus.findAll({
+          where: {
+            name: {
+              $in: availableStates,
+            },
+          },
+        });
+        const statuses = [...availableStatuses, task.status].map(status => ({
+          value: status.id,
+          text: status.name,
+          selected: status.id === task.status.id,
+        }));
+        log('Statuses: %o', statuses);
+        ctx.render('tasks/edit', { f: buildFormObj(task), task, users, tags, statuses });
+      } else {
+        ctx.throw(404);
+      }
+    })
+
+    .get('task', '/tasks/:id', async (ctx) => {
+      const task = await Task.findOne({
+        include: [
+          { model: User, as: 'creator' },
+          { model: User, as: 'assignedTo' },
+          { model: TaskStatus, as: 'status' },
+          { model: Tag },
+        ],
+        where: {
+          id: ctx.params.id,
+        },
+      });
+      if (task) {
+        ctx.render('tasks/task', { task });
+      } else {
+        ctx.throw(404);
+      }
     })
 
     .post('tasks', '/tasks', async (ctx) => {
@@ -130,8 +219,99 @@ export default (router, { Task, User, Tag, TaskStatus, logger }) => {
         ctx.flash.set('Task has been created!');
         ctx.redirect(router.url('root'));
       } catch (e) {
-        logger(e);
+        log(e);
         ctx.render('tasks/new', { f: buildFormObj(task, e) });
+      }
+    })
+
+    .patch('task', '/tasks/:id', async (ctx) => {
+      if (!ctx.state.isSignedIn()) {
+        ctx.throw(401);
+        return;
+      }
+      const form = ctx.request.body.form;
+      log('Recieved post data to patch: %o', form);
+      const taskKeys = Object.keys(form).filter(key =>
+        !_.includes(['assignedTo', 'tags', 'newTags', 'status'], key));
+      log('Keys for task: %o', taskKeys);
+      const taskBuild = taskKeys.reduce((acc, key) => {
+        acc[key] = form[key];
+        return acc;
+      }, {});
+      log('Task build object: %o', taskBuild);
+
+      const assignedUser = await User.findOne({
+        where: {
+          id: form.assignedTo,
+        },
+      });
+      const status = await TaskStatus.findOne({
+        where: {
+          id: form.status,
+        },
+      });
+
+      const newTags = (form.newTags instanceof Array ? form.newTags : [form.newTags])
+        .reduce((acc, name) => {
+          if (name) {
+            return [...acc, Tag.build({ name })];
+          }
+          return acc;
+        }, []);
+      // log('Build objects for new tags: %o', newTags);
+      const selectedTags = await Tag.findAll({
+        where: {
+          id: {
+            $in: form.tags instanceof Array ? form.tags : [form.tags],
+          },
+        },
+      });
+
+      try {
+        if (newTags) {
+          await Promise.all(newTags.map(tag => tag.save()));
+        }
+        await Task.update(
+          {
+            ...taskBuild,
+          },
+          {
+            where: {
+              id: ctx.params.id,
+            },
+          });
+        const task = await Task.findOne({
+          where: {
+            id: ctx.params.id,
+          },
+        });
+        await task.setTags([...selectedTags, ...newTags]);
+        await task.setAssignedTo(assignedUser);
+        await task.setStatus(status);
+        ctx.flash.set('Task has been updated!');
+        ctx.redirect(router.url('tasks'));
+      } catch (e) {
+        log(e);
+        ctx.redirect(router.url('tasks'));
+      }
+    })
+
+    .delete('task', '/tasks/:id', async (ctx) => {
+      if (!ctx.state.isSignedIn()) {
+        ctx.throw(401);
+        return;
+      }
+      const task = await Task.findOne({
+        where: {
+          id: ctx.params.id,
+        },
+      });
+      if (task) {
+        await task.destroy();
+        ctx.flash.set('Task has been deleted!');
+        ctx.redirect(router.url('tasks'));
+      } else {
+        ctx.throw(404);
       }
     });
 };
